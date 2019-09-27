87a474860d8062985a1c55f9fa8aa3aebe0c1a45
liuzhzh <liuzhzh@etutech.com>
2019-09-27 11:17:33 +0800

    update

--- RxPaginator/Classes/Helpers/Observable+Operators.swift
+++ RxPaginator/Classes/Helpers/Observable+Operators.swift
@@ -11,34 +11,34 @@ import RxCocoa
 
 extension ObservableType {
     
-    func filter<O: ObservableType>(_ predicate: O) -> Observable<E> where O.E == Bool {
+    func filter<O: ObservableType>(_ predicate: O) -> Observable<Element> where O.Element == Bool {
         return self
             .withLatestFrom(predicate) { element, predicate in (element, predicate) }
             .filter { _, predicate in predicate }
             .map { element, _ in element }
     }
     
-    func filterNot<O: ObservableType>(_ predicate: O) -> Observable<E> where O.E == Bool {
+    func filterNot<O: ObservableType>(_ predicate: O) -> Observable<Element> where O.Element == Bool {
         return self
             .withLatestFrom(predicate) { element, predicate in (element, predicate) }
             .filter { _, predicate in !predicate }
             .map { element, _ in element }
     }
     
-    func unwrap<T>() -> Observable<T> where E == T? {
+    func unwrap<T>() -> Observable<T> where Element == T? {
         return filter { $0 != nil }.map { $0! }
     }
 }
 
 extension SharedSequence {
     
-    func filter<O: SharedSequenceConvertibleType>(_ predicate: O) -> SharedSequence<S, E> where O.E == Bool, O.SharingStrategy == S {
+    func filter<O: SharedSequenceConvertibleType>(_ predicate: O) -> SharedSequence<SharingStrategy, Element> where O.Element == Bool, O.SharingStrategy == SharingStrategy {
         return withLatestFrom(predicate) { element, predicate in (element, predicate) }
             .filter { _, predicate in predicate }
             .map { element, _ in element }
     }
     
-    func filterNot<O: SharedSequenceConvertibleType>(_ predicate: O) -> SharedSequence<S, E> where O.E == Bool, O.SharingStrategy == S {
+    func filterNot<O: SharedSequenceConvertibleType>(_ predicate: O) -> SharedSequence<SharingStrategy, Element> where O.Element == Bool, O.SharingStrategy == SharingStrategy {
         return withLatestFrom(predicate) { element, predicate in (element, predicate) }
             .filter { _, predicate in !predicate }
             .map { element, _ in element }
